87d4a06d0c45c3e26a1ba7a801aca81794a011de
leviosa-1 <119951206+leviosa-1@users.noreply.github.com>
2023-07-14 12:37:58 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,54 @@
+let celciusInput = document.querySelector('#celcius >input')
+let fahrenheitInput = document.querySelector('#fahrenheit >input')
+let kelvinInput = document.querySelector('#kelvin >input')
+
+let btn = document.querySelector('.button button')
+
+
+function roundNumber(number){
+  return Math.round(number*100)/100
+}
+// celcius to fahrenite and kelvin
+
+celciusInput.addEventListener('input', function(){
+  let cTemp = parseFloat(celciusInput.value)
+  let fTemp = (cTemp*(9/5)) + 32
+  let kTemp = cTemp + 273.15
+
+  fahrenheitInput.value = roundNumber(fTemp)
+  kelvinInput.value = roundNumber(kTemp)
+
+})
+
+// fahrenite to celcius and kelvin 
+fahrenheitInput.addEventListener('input', function(){
+  let fTemp = parseFloat(fahrenheitInput.value)
+  let cTempTemp = (fTemp - 32) * (5/9)
+  let kTemp = (fTemp - 32) *(5/9) + 273.15
+
+  celciusInput.value = roundNumber(cTempTemp)
+  kelvinInput.value = roundNumber(kTemp)
+
+})
+
+// kelvin to celcius and fehranite 
+
+ 
+kelvinInput.addEventListener('input', function(){
+  let kTemp = parseFloat(kelvinInput.value)
+  let cTempTemp = kTemp - 273.15
+  let fTemp = (kTemp - 273.15 ) * (9/5) + 32 
+
+  celciusInput.value = roundNumber(cTempTemp)
+  fahrenheitInput.value = roundNumber(fTemp)
+
+})
+
+
+
+btn.addEventListener('click', ()=>{
+  celciusInput.value =""
+  fahrenheitInput.value = ""
+  kelvinInput.value =""
+})
+
